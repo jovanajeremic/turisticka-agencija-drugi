b2aabb926a1c03c67c5a14c36d2916850a687c36
Jovana Jeremic <jovana00jeremic@gmail.com>
2023-08-23 23:17:18 +0200

    Api ruta

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,29 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post("login", "App\Http\Controllers\UserController@login");
+Route::post("register", "App\Http\Controllers\UserController@register");
+
+
+//sancturm middleware
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::get("tip", "App\Http\Controllers\TipController@index");
+    Route::get("tip/{id}", "App\Http\Controllers\TipController@find");
+    Route::post("tip", "App\Http\Controllers\TipController@create");
+    Route::put("tip/{id}", "App\Http\Controllers\TipController@update");
+    Route::delete("tip/{id}", "App\Http\Controllers\TipController@delete");
+    Route::get("drzava", "App\Http\Controllers\DrzavaController@index");
+    Route::get("drzava/{id}", "App\Http\Controllers\DrzavaController@find");
+    Route::post("drzava", "App\Http\Controllers\DrzavaController@create");
+    Route::put("drzava/{id}", "App\Http\Controllers\DrzavaController@update");
+    Route::delete("drzava/{id}", "App\Http\Controllers\DrzavaController@delete");
+    Route::get("ponuda", "App\Http\Controllers\PonudaController@index");
+    Route::get("ponuda/{id}", "App\Http\Controllers\PonudaController@find");
+    Route::post("ponuda", "App\Http\Controllers\PonudaController@create");
+    Route::put("ponuda/{id}", "App\Http\Controllers\PonudaController@update");
+    Route::delete("ponuda/{id}", "App\Http\Controllers\PonudaController@delete");
+
+    Route::get("logout", "App\Http\Controllers\UserController@logout");
+});
